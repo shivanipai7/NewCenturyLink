9319b57e33f1e3815e6c273fd756137d164c9eed
Nidhi <nidhi.kedia557@gmail.com>
2019-11-06 12:49:44 +0530

    My First commit

---
+++ GitProject/src/com/training/Application.java
@@ -0,0 +1,13 @@
+package com.training;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Application {
+	public static void main(String[] args) {
+	
+	List<String> nameList = Arrays.asList("Ramesh");
+	nameList.forEach(System.out::println);
+
+}
+}
